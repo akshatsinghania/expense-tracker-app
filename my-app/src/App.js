35d0567e445e5a4b38bfd99fe1b4159d7c48ed4f
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 03:51:16 +0530

    added some of the upi's

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+      <Flex height={"full"}>
+        <Box height={"full"} Flex={5}>
+          <Main />
+        </Box>
+      </Flex>
+    </Container>
   );
 }
 
