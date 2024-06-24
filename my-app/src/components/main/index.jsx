84457c2583efb5facfab961a73e974c90aaa03f2
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 04:26:11 +0530

    added some more ui funtions

---
+++ my-app/src/components/main/index.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
+      <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
+        <Heading></Heading>
+      </Flex>
+    </Flex>
+  );
+}
--- my-app/src/context/index.jsx
+++ my-app/src/context/index.jsx
@@ -3,5 +3,5 @@ import { createContext } from "react";
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-  return <GlobalContext.Provider>{children}</GlobalContext.Provider>;
+  return <GlobalContext.Provider value={[]}>{children}</GlobalContext.Provider>;
 }
