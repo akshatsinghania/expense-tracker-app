b02581f5308461c0315d83b4064dad6f993c89d8
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 13:41:20 +0530

    partially added the heading

---
+++ my-app/src/components/summary/index.jsx
@@ -0,0 +1,42 @@
+import { Box, Flex, Heading } from "@chakra-ui/react";
+
+export default function Summary() {
+  return (
+    <Box
+      p="6"
+      border={"1px solid"}
+      borderColor={"gray.100"}
+      overflow={"hidden"}
+      borderRadius={"10"}
+      background={"white"}
+      display={"flex"}
+    >
+      <Flex
+        w="full"
+        justifyContent={"center"}
+        alignItems={"center"}
+        flexDirection={{
+          base: "column",
+          sm: "column",
+          md: "column",
+          lg: "row",
+          xl: "row",
+        }}
+      >
+        <Flex
+          flex={1}
+          w={"full"}
+          flexDirection={"column"}
+          alignItems={"center"}
+          justifyContent={"space-evenly"}
+          ml={"-20"}
+          mr={"2"}
+        >
+          <Heading size={"md"} mb={"4"} color={"grey.600"}>
+            Balance is 100
+          </Heading>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
