c564b81b38e25c0cd8f8e48fb42617e9acb4f5b7
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 15:16:51 +0530

    added pie chart

--- my-app/src/context/index.jsx
+++ my-app/src/context/index.jsx
@@ -1,7 +1,34 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-  return <GlobalContext.Provider value={[]}>{children}</GlobalContext.Provider>;
+  const [formData, setFormData] = useState({
+    type: "expense",
+    amount: 0,
+    description: "",
+  });
+
+  const [value, setValue] = useState("expense");
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalIncome, setTotalIncome] = useState(0);
+  const [allTransactions, setAllTransactions] = useState([]);
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalIncome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransactions,
+        setAllTransactions,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
