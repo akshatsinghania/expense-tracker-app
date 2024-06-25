ae30d6b6040e192ee540e9f7895d2eee9d386d96
aksinghania <akshtsinghnia@gmail.com>
2024-06-25 13:19:34 +0530

    added some of the elements

--- my-app/src/components/main/index.jsx
+++ my-app/src/components/main/index.jsx
@@ -1,8 +1,20 @@
+import { Button, Flex, Heading } from "@chakra-ui/react";
+
 export default function Main() {
   return (
     <Flex textAlign={"center"} flexDirection={"column"} pr={"5"} pl={"5"}>
       <Flex alignItems={"center"} justifyContent={"space-between"} mt={"12"}>
-        <Heading></Heading>
+        <Heading
+          color={"blue.400"}
+          display={["none", "block", "block", "block", "block"]}
+        >
+          Expense Tracker
+        </Heading>
+        <Flex alignItems={"center"}>
+          <Button bg={"blue.500"} color={"black"} ml={"4"}>
+            Add new Transaction
+          </Button>
+        </Flex>
       </Flex>
     </Flex>
   );
